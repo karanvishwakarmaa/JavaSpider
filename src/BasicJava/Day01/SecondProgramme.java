9d7168dc5c93e6fd9dd8978c1ca4e183e5ff87e3
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-06-28 15:54:38 +0530

    done second programme

---
+++ src/BasicJava/Day01/SecondProgramme.java
@@ -0,0 +1,11 @@
+package BasicJava.Day01;
+
+public class SecondProgramme {
+    public static void main(String[] args) {
+        System.out.println(12345);
+        System.out.println(65.4);
+        System.out.println('t');
+        System.out.println("Karann");
+        System.out.println(true);
+    }
+}
