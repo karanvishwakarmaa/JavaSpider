2f69162d56e547910c34c9c275cc2e7dcede36bc
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-06-28 15:50:55 +0530

    starting java

--- src/BasicJava/Day01/firstProgramme.java
+++ src/BasicJava/Day01/firstProgramme.java
@@ -2,7 +2,7 @@ package BasicJava.Day01;
 
 public class firstProgramme {
     public static void main(String[] args) {
-        System.out.println("Hey! I'm Royalkarann");
+        System.out.println("Hey! I'm Royalkarann from benglore");
     }
 }
 
